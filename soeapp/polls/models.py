caabe52ae32a920aaedbf60341efd877ecddca5a
KR-Soe <swoordeath@gmail.com>
2019-11-05 01:10:08 -0300

    first db model created

---
+++ soeapp/polls/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Player(models.Model):
+  user = models.CharField(max_length=15)
+  summoner_name = models.CharField(max_length=30)
+  password = models.CharField(max_length=20)
+  email = models.CharField(max_length=70)
+  
+class Champ(models.Model):
+  name = models.CharField(max_length=30)
+  lore = models.CharField(max_length=500)
+  
+class Skin(models.Model):
+  name = models.CharField(max_length=20)
+  champ = models.ForeignKey(Champ, on_delete=models.CASCADE)
+
--- soeapp/soeapp/settings.py
+++ soeapp/soeapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,8 @@ WSGI_APPLICATION = 'soeapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'NAME': '/home/soe/Documentos/develop/soe-app/db.sqlite3'
     }
 }
 
