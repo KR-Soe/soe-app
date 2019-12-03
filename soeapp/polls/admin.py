de25caf04784193898dd3dfbf18e05ec2b2dfcbe
KR-Soe <swoordeath@gmail.com>
2019-12-03 16:16:01 -0300

    modified admin model from django and webpack config installed to the project

--- soeapp/polls/admin.py
+++ soeapp/polls/admin.py
@@ -1,6 +1,26 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import Player, Champ, Skin
 
-# Register your models here.
+@admin.register(Player)
+class AdminPlayer(admin.ModelAdmin):
+  list_display = (
+    'id',
+    'user',
+    'summoner_name',
+    'password',
+    'email'
+  )
+  
+@admin.register(Champ)
+class AdminChamp(admin.ModelAdmin):
+  list_display = (
+    'name',
+    'lore'
+  )
+  
+@admin.register(Skin)
+class AdminSkin(admin.ModelAdmin):
+  list_display = (
+    'name',
+    'champ_id'
+  )
--- soeapp/polls/models.py
+++ soeapp/polls/models.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
 class Player(models.Model):
@@ -9,9 +7,15 @@ class Player(models.Model):
   password = models.CharField(max_length=20)
   email = models.CharField(max_length=70)
   
+  def __str__(self):
+    return self.user
+  
 class Champ(models.Model):
   name = models.CharField(max_length=30)
   lore = models.CharField(max_length=500)
+  
+  def __str__(self):
+    return self.name
   
 class Skin(models.Model):
   name = models.CharField(max_length=20)
